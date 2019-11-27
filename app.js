1ecf7f774267e5f612205d248e98b6d4a07ccd6f
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2019-11-27 13:26:38 -0500

    Add CSS to checklist

--- app.js
+++ app.js
@@ -3,30 +3,31 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
-var items = []
+let items = []
 
 app.use(bodyParser.urlencoded ({extended:true}))
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
 app.get("/", function(req,res) {
 
-  var today = new Date();
+  let today = new Date();
   
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   }
 
-  var day = today.toLocaleDateString("en-US", options)
+  let day = today.toLocaleDateString("en-US", options)
 
   res.render("lists", {kindOfDay: day, newListItem: items})
   
 })
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem
+  let item = req.body.newItem
 
   items.push(item)
 
