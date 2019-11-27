5c904d78428f99521c2967872f0172a2230d875e
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2019-11-27 17:23:04 -0500

    Finish To Do List v1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const date = require(__dirname + '/date.js')
 
 const app = express()
 
 let items = []
+let workItems = []
 
 app.use(bodyParser.urlencoded ({extended:true}))
 app.use(express.static('public'))
@@ -12,29 +14,40 @@ app.set('view engine', 'ejs')
 
 app.get("/", function(req,res) {
 
-  let today = new Date();
-  
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }
+  let day = date()
 
-  let day = today.toLocaleDateString("en-US", options)
-
-  res.render("lists", {kindOfDay: day, newListItem: items})
+  res.render("lists", {listTitle: day, newListItem: items})
   
 })
 
 app.post("/", function(req, res) {
+
   let item = req.body.newItem
 
-  items.push(item)
-
-  res.redirect("/")
-  
+  if (req.body.list === "Work") {
+    workItems.push(item)
+    res.redirect('/work')
+  } else {
+    items.push(item)
+    res.redirect('/')
+  }  
 })
 
+
+app.get("/work", function(req,res) {
+  res.render('lists', {listTitle: 'Work List', newListItem: workItems})
+})
+
+
+app.post('/work', function (req,res){
+  let item = req.body.newItem;
+  workItems.push(item)
+  res.redirect('/work')
+})
+
+app.get('/about', function (req,res) {
+  res.render('about')
+})
 
 
 app.listen(3000, function() {
